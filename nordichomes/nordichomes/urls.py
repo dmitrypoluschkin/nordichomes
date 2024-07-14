75cacadcff011fda67880ef2c81097e005059673
unknown <19polushkin.dima87@gmail.com>
2024-07-14 12:48:47 +0400

    logout error

--- nordichomes/nordichomes/urls.py
+++ nordichomes/nordichomes/urls.py
@@ -1,18 +1,18 @@
 from django.conf import settings
-from django.contrib import admin
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.contrib.auth import views
 from django.urls import path
 
 from cart.views import add_to_cart
-from core.views import frontpage, shop, signup, login_old, logout
+from core.views import frontpage, shop, signup
 from product.views import product
 
 urlpatterns = [
     path('', frontpage, name="frontpage"),
     path('signup/', signup, name="signup" ),
-    path('loguot/', views.LogoutView.as_view(), name='logout'),
-    path('login/', login_old, name='login_old'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login_old'),
     path('shop/', shop, name="shop"),
     path('shop/<slug:slug>/', product, name="product"),
     path('add_to_cart/<int:product_id>/', add_to_cart, name="add_to_cart"),
