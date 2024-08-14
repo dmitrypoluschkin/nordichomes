86e7fa4f4203bb2c4cb2c859680570166be7d19f
unknown <19polushkin.dima87@gmail.com>
2024-08-14 20:00:51 +0400

    add order

--- nordichomes/nordichomes/urls.py
+++ nordichomes/nordichomes/urls.py
@@ -8,6 +8,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('core.urls')),
     path('cart/', include('cart.urls')),
+    path('order/', include('order.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
