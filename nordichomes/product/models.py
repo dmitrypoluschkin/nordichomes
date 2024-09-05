335aac62a308092e7b5af62886585f2238218fd9
unknown <19polushkin.dima87@gmail.com>
2024-09-06 00:12:36 +0400

    add rating

--- nordichomes/product/models.py
+++ nordichomes/product/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.core.files import File
 
@@ -60,3 +61,22 @@ class Product(models.Model):
 
         return thumbnail
 
+    
+    def get_rating(self):
+        reviews_total = 0
+
+        for review in self.reviews.all():
+            reviews_total += review.rating
+
+        if reviews_total > 0:
+            return reviews_total / self.reviews.count()
+
+        return 0
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+    rating = models.IntegerField(default=3)
+    content = models.TextField()
+    created_by = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
